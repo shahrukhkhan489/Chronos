c6bd29af5661c6a4f822775df0b66541782e9055
timelyart <timelyart@protonmail.com>
2020-04-08 17:53:15 +0200

    Added an easy method to synchronize the database, i.e. db init, db migrate, db upgrade and default database records (seed) Create database if it doesn't exist, and synchronize the database

--- chronos/__init__.py
+++ chronos/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_migrate import Migrate, MigrateCommand
 from flask_nav import Nav
@@ -5,14 +7,29 @@ from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
+from sqlalchemy.sql import ClauseElement
 from chronos.libs import tools
 from chronos.web.http_error import page_not_found
 
+
+def init_db(database):
+
+    """Add a save() function to db.Model"""
+    def save(model):
+        try:
+            database.session.add(model)
+            database.session.commit()
+        except Exception as e:
+            log.exception(e)
+
+    database.Model.save = save
+
+
 db = SQLAlchemy()
+init_db(db)
 migrate = Migrate()
 manager = Manager()
 nav = Nav()
-
 config = tools.get_config()
 log = tools.create_log()
 log.setLevel(20)
@@ -25,14 +42,21 @@ def create_app():
     if config.has_option('security', 'webserver_password'):
         app.config['SECRET_KEY'] = config.get('security', 'webserver_password')
     if config.has_option('database', 'connection_string'):
-        app.config['SQLALCHEMY_DATABASE_URI'] = config.get('database', 'connection_string')
+        from sqlalchemy_utils import create_database, database_exists
+        connection_string = config.get('database', 'connection_string')
+        app.config['SQLALCHEMY_DATABASE_URI'] = connection_string
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['DATABASE_CREATED'] = False
+        if not database_exists(connection_string):
+            create_database(connection_string)
+            app.config['DATABASE_CREATED'] = True
+
     # app.config.from_object('config.Config')
     app.register_error_handler(404, page_not_found)
 
     # Initialize Plugins
     db.init_app(app)
-    migrate.init_app(app, db)
+    migrate.init_app(app, db, directory=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'migrations'))
     manager.__init__(app)
     manager.add_command('db', MigrateCommand)
     nav.init_app(app)
--- manage.py
+++ manage.py
@@ -1,9 +1,90 @@
 """
 This file is here to be able to do updates to the database without reverting to DROP TABLE, i.e. to use Flask-Migrate
 """
-from chronos import create_app
+import os
+import subprocess
+from chronos import create_app, manager, log
 
 app = create_app()
+
+
+def seed():
+    """Add seed data to the database."""
+    from chronos.model import Exchange, get_or_create
+    print("Updating records... ", end="")
+    # Exchanges
+    exchange = get_or_create(Exchange, name='Kraken')
+    exchange.ccxt_name = 'kraken'
+    exchange.class_name = None
+    exchange.save()
+    exchange = get_or_create(Exchange, name='Kraken Futures')
+    exchange.ccxt_name = None
+    exchange.class_name = 'KrakenFutures'
+    exchange.save()
+    print("OK")
+
+
+def init():
+    file = os.path.abspath(__file__)
+    try:
+        print("Setting up Flask-Migrate ... ")
+        result = subprocess.run(['python', file, 'db', 'init'], shell=True)
+        if result.returncode == 0:
+            print("OK")
+        else:
+            print("FAILED")
+    except Exception as e:
+        log.exception(e)
+
+
+def migrate():
+    file = os.path.abspath(__file__)
+    try:
+        print("Generating migration script from model... ")
+        # result = subprocess.run(['dir'], shell=True)
+        result = subprocess.run(['python', file, 'db', 'migrate'], shell=True)
+        if result.returncode == 0:
+            print("OK")
+        else:
+            print("FAILED")
+    except Exception as e:
+        log.exception(e)
+
+
+def upgrade():
+    file = os.path.abspath(__file__)
+    try:
+        print("Executing migration script to database... ")
+        result = subprocess.run(['python', file, 'db', 'upgrade'], shell=True)
+        if result.returncode == 0:
+            print("OK")
+        else:
+            print("FAILED")
+    except Exception as e:
+        log.exception(e)
+
+
+@manager.command
+def update():
+    migrate()
+    upgrade()
+    seed()
+    print("===============================")
+    print("Database Update Complete")
+
+
+def first_time_setup():
+    init()
+    file = os.path.abspath(__file__)
+    try:
+        subprocess.run(['python', file, 'db', 'update'], shell=True)
+    except Exception as e:
+        log.exception(e)
+
+
+if app.config['DATABASE_CREATED']:
+    first_time_setup()
+
 if __name__ == '__main__':
     from chronos import model
     model.manage()
